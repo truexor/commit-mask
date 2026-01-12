fb929bff4aa48f8dd960ab455a854f6b302391b8
truexor <am.kushal02@gmail.com>
2026-01-13 03:52:13 +0530

    logs format

--- utils/logs.go
+++ utils/logs.go
@@ -2,18 +2,18 @@ package utils
 
 import "fmt"
 
-func LogInfo(msg string) {
-	fmt.Println(msg)
-}
-
 func LogSuccess(msg string) {
-	fmt.Println(msg)
-}
-
-func LogWarning(msg string) {
-	fmt.Println(msg)
+	fmt.Println("✅ \033[32m" + msg + "\033[0m")
 }
 
 func LogUpdate(msg string) {
-	fmt.Println(msg)
+	fmt.Println("🔄 \033[34m" + msg + "\033[0m")
+}
+
+func LogInfo(msg string) {
+	fmt.Println("- \033[90m" + msg + "\033[0m")
+}
+
+func LogWarning(msg string) {
+	fmt.Println("⚠️ \033[33m" + msg + "\033[0m")
 }
